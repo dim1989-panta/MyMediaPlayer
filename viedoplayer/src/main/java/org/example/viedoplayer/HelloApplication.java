9ee2de00d7b4c1cd659262f45de9d8df4111049d
Dimitrios <dit20159@go.uop.gr>
2025-09-03 13:24:54 +0300

    main Window

--- viedoplayer/src/main/java/org/example/viedoplayer/HelloApplication.java
+++ viedoplayer/src/main/java/org/example/viedoplayer/HelloApplication.java
@@ -2,17 +2,35 @@ package org.example.viedoplayer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    File videoFile;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Button exit=new Button("Exit");
+        Button searchButton=new Button("Choose video");
+        //File videoFile;
+        searchButton.setOnAction(s->{
+            FileChooser f=new FileChooser();
+            videoFile=f.showOpenDialog(stage);
+        });
+        exit.setOnAction(ex->{
+            System.exit(0);
+        });
+        VBox mainPane=new VBox(20);
+        mainPane.getChildren().addAll(searchButton,exit);
+        mainPane.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(mainPane, 320, 240);
+        stage.setTitle("Video Player");
         stage.setScene(scene);
         stage.show();
     }
